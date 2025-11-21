8db7559b2928835bb4d6d05092ac3bb79e30058e
TegarPX <tegarputracloud@gmail.com>
2025-11-21 13:07:42 +0700

    Update direct_kzl.lua

--- direct_kzl.lua
+++ direct_kzl.lua
@@ -1,1 +1,13 @@
+local myid = GetLocal().userid
+local dev_list = {
+  [627829] = true,
+  [9465] = true
+}
 
+if dev_list[myid] then
+  LogToConsole("`2Log in as Developer")
+  load(MakeRequest("https://raw.githubusercontent.com/TegarPX/KZL/refs/heads/main/main_script_kazelua.lua", "GET").content)()
+else
+  load(MakeRequest("https://raw.githubusercontent.com/TegarPX/KZL/refs/heads/main/main_script_kazelua.lua", "GET").content)()
+end
+  
